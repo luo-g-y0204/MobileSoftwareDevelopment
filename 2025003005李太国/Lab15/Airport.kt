9b5255267434c444880d3c9547ec07d00196253c
happymoliang <3348342191@qq.com>
2026-06-15 12:46:09 +0800

    完成Lab15

---
+++ 2025003005李太国/Lab15/Airport.kt
@@ -0,0 +1,13 @@
+package com.example.flightsearch
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "airport")
+data class Airport(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val iata_code: String,
+    val name: String,
+    val passengers: Int
+)
