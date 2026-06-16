5bf14c6f28f43f3e4ad73a87a6f6f899f0a47452
1119yu <2541505945@qq.com>
2026-06-16 20:13:13 +0800

    完成Lab15作业

---
+++ 2025003017玉旺叫/Lab15/FlightDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.flightsearch
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Airport::class, Favorite::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class FlightDatabase : RoomDatabase() {
+    abstract fun flightDao(): FlightDao
+
+    companion object {
+        @Volatile
+        private var Instance: FlightDatabase? = null
+
+        fun getDatabase(context: Context): FlightDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    FlightDatabase::class.java,
+                    "flight_search_database"
+                )
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
